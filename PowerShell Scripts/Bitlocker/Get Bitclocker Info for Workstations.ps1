45f69d500dcc29ee6770af23a4cd931856098f6a
cjcombsMediaCJ Combs <167232782+CJ-ITS-NOC@users.noreply.github.com>
2024-08-15 10:16:25 -0400

    new scripts

---
+++ PowerShell Scripts/Bitlocker/Get Bitclocker Info for Workstations.ps1
@@ -0,0 +1,64 @@
+﻿#Get the Bitlocker Version
+$bitlockerStatus = manage-bde -status
+$sections = $bitlockerStatus -split "`r`n`r`n`r`n`r`n`r`n`r`n`r`n`r`n"
+$BitLockerVersion = $sections | Select-String -Pattern "Bitlocker Version:"
+$BitLockerVersion = ($BitLockerVersion -split "Bitlocker Version:")[1].Trim()
+$BitlockerVersion
+
+
+#get Bitlocker Encryption Method
+$bitlockerStatus = manage-bde -status
+$sections = $bitlockerStatus -split "`r`n`r`n`r`n`r`n`r`n`r`n`r`n`r`n"
+$EncryptionMethod = $sections | Select-String -Pattern "Encryption Method:"
+
+$EncryptionMethod = ($EncryptionMethod -split "Encryption Method:")[1].Trim()
+$EncryptionMethod
+
+#Get Lock status
+$bitlockerStatus = manage-bde -status
+$sections = $bitlockerStatus -split "`r`n`r`n`r`n`r`n`r`n`r`n`r`n`r`n"
+$LockStatus       = $sections | Select-String -Pattern "Lock Status:"
+$LockStatus = ($LockStatus -split "Lock Status:")[1].Trim()
+$LockStatus
+
+#Get Protection Status
+$bitlockerStatus = manage-bde -status
+$sections = $bitlockerStatus -split "`r`n`r`n`r`n`r`n`r`n`r`n`r`n`r`n"
+$ProtectionStatus = $sections | Select-String -Pattern "Protection Status:"
+$ProtectionStatus = ($ProtectionStatus -split "Protection Status:")[1].Trim()
+$ProtectionStatus
+
+
+#Get Sophos Device Encryption Service
+
+$serviceName = "Sophos Device Encryption Service"
+
+# Check if the service is installed
+if (Get-Service -Name $serviceName -ErrorAction SilentlyContinue) {
+    
+    Write-Host "Yes Sophos Encryption is Enabled"
+} else {
+  
+    Write-Host "No Sophos Protection"
+}
+
+
+#Get the recovery password. we have to save it to a text file first in order for us to get the details that we need, otherwise powershell script will not be able to pull the line 3
+manage-bde -protectors -get c: -t RecoveryPassword >  C:\windows\ltsvc\packages\bitl.txt
+$FilePath = "C:\windows\ltsvc\packages\bitl.txt"
+$lineNumber = 8
+$NP_ID = (Get-Content -Path $FilePath | Select-Object -Index ($lineNumber-1))
+$NP_ID
+
+
+#Get the Numerical Password
+$FilePath = "C:\windows\ltsvc\packages\bitl.txt"
+$lineNumber = 8
+$NP_ID = (Get-Content -Path $FilePath | Select-Object -Index ($lineNumber-1))
+$NP_ID
+
+#Get Bitlocker Numerical Password
+$FilePath = "C:\windows\ltsvc\packages\bitl.txt"
+$lineNumber = 10
+$NP_ID = (Get-Content -Path $FilePath | Select-Object -Index ($lineNumber-1))
+$NP_ID
---
+++ PowerShell Scripts/Internet Explorer/Set IE to Automatic Detect Settings.ps1
@@ -0,0 +1,15 @@
+﻿#Get the IE Autodect information
+$regPath = "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
+$regName = "AutoDetect"
+
+# Get the current value of the registry key
+$currentValue = Get-ItemProperty -Path $regPath -Name $regName | Select-Object -ExpandProperty $regName
+
+# Check if the current value is 0
+if ($currentValue -eq 0) {
+    # Set the registry value to 1
+    Set-ItemProperty -Path $regPath -Name $regName -Value 1
+    Write-Output "AutoDetect was set to 0. It has been updated to 1."
+} else {
+    Write-Output "AutoDetect is already set to 1 or another value. No changes made."
+}
